8a82dcbfcb3b270d97341b4ae8113cf1f5be90da
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-08-27 10:43:37 +0200

    add nix flake for develop

---
+++ README.md
@@ -0,0 +1,8 @@
+# 
+
+## Set up
+```shell
+$ git clone https://github.com/jtremesay/eda.git eda
+$ cd eda
+$ nix develop
+```
---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    openroad = {
+      type = "git";
+      url = "https://github.com/The-OpenROAD-Project/OpenROAD";
+      submodules = true;
+    };
+    yosys = {
+      type = "git";
+      url = "https://github.com/The-OpenROAD-Project/yosys";
+      submodules = true;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    # openroad-flow = {
+    #   type = "git";
+    #   url = "https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts";
+    #   submodules = true;
+    # };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      openroad,
+      yosys,
+      #openroad-flow,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            openroad.packages.${system}.default
+            yosys.packages.${system}.default
+          ];
+          packages = with pkgs; [
+            fish
+            nil
+            nixd
+            nixfmt-rfc-style
+            time
+            klayout
+            verilator
+            perl
+            python3
+            python3Packages.pandas
+            python3Packages.numpy
+            python3Packages.firebase-admin
+            python3Packages.click
+            python3Packages.pyyaml
+            python3Packages.yamlfix
+          ];
+          shellHook = ''
+            exec ${pkgs.fish}/bin/fish
+          '';
+        };
+      }
+    );
+}
